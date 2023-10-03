82ab0611a4eb73287d13f5ebd1396b88a716d12e
Reda Hmeid <redahmeid@redas-mbp.lan>
2023-10-03 07:47:17 +0100

    create club, dummy function

--- handler.py
+++ handler.py
@@ -1,12 +1,21 @@
 import json
+from pydantic import TypeAdapter, ValidationError
+from classes import Club
 
-
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
+def create_club(event, context):
+    body =json.loads(event["body"])
+    ClubValidator = TypeAdapter(Club)
+    print(body)
+    try:
+        new_club = ClubValidator.validate_python(body)
+        save_club(new_club)
+        response = {"statusCode": 200, "body": new_club.model_dump_json()}
+        print(response)
+    except ValidationError as e:
+        print(e)
+        response = {"statusCode": 400, "body": "invalid club"}
+    
     return response
+def save_club(club:Club):
+    print("here")
+    # save club
